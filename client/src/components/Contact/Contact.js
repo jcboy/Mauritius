d7d8a1dd3a847a9579ff51aa55e905b1b0b94c5b
LENOVO <hadi.jamalahmad@gmail.com>
2021-01-06 19:02:09 +0100

    B

---
+++ client/src/components/Contact/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Component} from 'react';
+
+class Form extends Component {
+
+
+      render() {
+
+          return <div>
+              <h1>Mauritius - Contact</h1>
+
+          </div>
+
+      }
+
+}
+
+export default Form;
+
