4f6915890b3a7ca7f3fda51418378375f10ca380
jcboyG4C <jcboy@goforcolor.com>
2021-01-08 23:04:15 +0100

    premier rendu page list activities

---
+++ client/src/pages/activities/AddCategory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import Select from 'react-select';
+
+export const AddCategory = () => {
+    return (
+        <div>
+            <input type="text"/>
+            <button type="button" className="btn btn-outline-primary ">Valider</button>
+        </div>
+    )
+}
