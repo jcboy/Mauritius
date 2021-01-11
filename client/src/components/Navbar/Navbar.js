457bf758b08919e983b0637ae4f7416187ce49e4
aigouassel <audreyigouassel@gmail.com>
2021-01-11 15:29:27 +0100

    création d'un fichier css et ajout du background bloc 1

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
-// import { Link, NavLink } from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import '../../styles/navbar.css';
 
 export const Navbar = () => {
     return (
-        <nav>
-            <div>Navbar</div>
+        <nav className="Nav row py-3">
+            <div className="NavBloc col-6">
+                <Link to="/mauritius">
+                    <h1> Mauritius </h1>
+                </Link>
+            </div>
+            <div className="NavBloc col-6">
+                <Link to="/" className="LinkStyle"> Accueil </Link>
+                <Link to="/activities" className="LinkStyle"> Que faire ? </Link>
+                <Link to="/news" className="LinkStyle"> Actualités </Link>
+            </div>
         </nav>
     )
 }
