0dc617f934acf9be138db94736508d7ff714a975
aigouassel <audreyigouassel@gmail.com>
2021-01-11 16:03:18 +0100

    mise en place de la barre de navigation et de l'image d'accueil "welcome to paradise"

---
+++ client/src/components/Welcome/welcome.js
@@ -0,0 +1,12 @@
+import '../../styles/styles.css'
+import '../../styles/welcome.css'
+
+export function Welcome() {
+    return (
+        <div className="row Welcome justify-content-center py-5">
+            <div className="col-4">
+                <h1 className="h1"> WELCOME TO PARADISE </h1>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Contact from "../../components/Contact/Contact";
+import {Welcome} from "../../components/Welcome/welcome";
 
 export const Home = () => {
     return (
         <div>
+            <Welcome/>
             <div className="container">
                 {/* Ceci est juste un exemple */}
                 <h1 className="h1 green text-center mb-5">Un titre header</h1>
                 <h2 className="h2 text-center mb-5">Un soutitre</h2>
                 <p>
-                    Latrones timore latrones praesidiis fore populationum magnis et propinqua stataria per diu per documentis intactam cum Lycaoniam congressione parte scirent igitur campestrem Lycaoniam inpares fore per petivere timore diu inpares timore campestrem congressione magnis milite scirent sed igitur et latrones
+                    Latrones timore latrones praesidiis fore populationum magnis et propinqua stataria per diu per
+                    documentis intactam cum Lycaoniam congressione parte scirent igitur campestrem Lycaoniam inpares
+                    fore per petivere timore diu inpares timore campestrem congressione magnis milite scirent sed igitur
+                    et latrones
                 </p>
                 <p>
-                    Inpares fore intactam undique parte parte magnis campestrem petivere milite fore magna per praesidiis
+                    Inpares fore intactam undique parte parte magnis campestrem petivere milite fore magna per
+                    praesidiis
                     igitur congressione se igitur deviis nostris populationum magnis timore undique Lycaoniam parte
                     frequentibus per caedium et magna per magnis cum parte stataria nostris stataria congressione diu
                     praesidiis undique igitur Pamphyliam undique frequentibus se deviis quidem caedium caedium latrones
