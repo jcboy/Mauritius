813b1cfecf81874314c83a3910df0d79710ac1b5
aigouassel <audreyigouassel@gmail.com>
2021-01-11 19:13:31 +0100

    mise en place de la barre de navigation et de l'image d'accueil "welcome to paradise"

--- client/src/components/Welcome/welcome.js
+++ client/src/components/Welcome/welcome.js
@@ -1,11 +1,16 @@
 import '../../styles/styles.css'
 import '../../styles/welcome.css'
+import {Navbar} from "../Navbar/Navbar";
+
 
 export function Welcome() {
     return (
-        <div className="row Welcome justify-content-center py-5">
-            <div className="col-4">
-                <h1 className="h1"> WELCOME TO PARADISE </h1>
+        <div>
+            <Navbar/>
+            <div className="row Welcome justify-content-center py-5">
+                <div className="col-4">
+                    <h1 className="h1"> WELCOME TO PARADISE </h1>
+                </div>
             </div>
         </div>
     )
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Redirect,
-} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import {Home} from "../pages/home/home";
-import {Navbar} from "../components/Navbar/Navbar";
 import ActivityList from "../pages/activities/ActivityList";
 
 export const AppRouter = () => {
     return (
         <Router>
             <div>
-                <Navbar />
                 <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route exact path="/activities" component={ActivityList} />
-
-                    <Redirect to="/" />
+                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/activities" component={ActivityList}/>
+                    <Redirect to="/"/>
                 </Switch>
             </div>
         </Router>
