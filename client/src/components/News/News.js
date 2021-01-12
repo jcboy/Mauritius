fe86a68943bebe96a86a611cc34482d2e4b78c8a
LENOVO <kayaam01@hotmail.fr>
2021-01-12 15:37:28 +0100

    add text

---
+++ client/src/components/News/News.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {Component} from "react";
+
+
+class TestTableau extends Component {
+
+    render() {
+        return <div>
+            <select id="monselect">
+                <option value="valeur1"> Ordre croissant</option>
+                <option value="valeur2" selected>Ordre décroissant</option>
+            </select>
+            <script type='javascript'>
+                let array = [];
+
+                array.push({id:1,  'actu','utca', 1997, 'oikdzfj'}),
+                {2,  'actu', 'utca', 1964, 'oikdzfj'},
+                {3, 'actu', 'utca', 1999, 'oikdzfj'},
+                {4,  'actu', 'utca', 1997, 'oikdzfj'}
+                ;
+
+
+                let temp = 1997;
+
+                array.date = temp |> array.map |> console.log
+            </script>
+        </div>
+    };
+}
+
+export default TestTableau;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
