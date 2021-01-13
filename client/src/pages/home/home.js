f4633c7e65d5776210004e5ed01b0e4e2e143d4c
SALAH <s.salaheddine91@gmail.com>
2021-01-13 10:47:40 +0100

    add text

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,41 +1,9 @@
 import React from 'react';
+import TextMap from "../../components/TextMap/TextMap";
 
 export const Home = () => {
     return (
-
-            <div className="container">
-                <h1 className="h1 green text-center mb-5">L'ILE MAURICE EN QUELQUES MOTS</h1>
-                <div className="row text">
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede.
-                        Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.
-                    </p>
-
-                    <p>
-                        Eos autem iusto cum ullam reiciendis sit veniam eligendi.
-                        Est minima autem aut reiciendis fugiat id velit ratione et perferendis assumenda qui rerum autem sit ipsam dolores.
-                        Quod si rectum statuerimus vel concedere amicis, quidquid velint, vel impetrare ab iis,
-                        quidquid velimus, perfecta quidem sapientia si simus, nihil habeat res vitii;
-                    </p>
-
-                    <p>
-                        Ut velit mauris, egestas sed, gravida nec, ornare ut, mi. Aenean ut orci vel massa suscipit pulvinar. Nulla sollicitudin.
-                        Fusce varius, ligula non tempus aliquam, nunc turpis ullamcorper nibh, in tempus sapien eros vitae ligula. Pellentesque rhoncus nunc et augue.
-                        Integer id felis. Curabitur aliquet pellentesque diam. Integer quis metus vitae elit lobortis egestas. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi vel erat non mauris convallis vehicula. Nulla et sapien. Integer tortor tellus, aliquam faucibus, convallis id, congue eu, quam. Mauris ullamcorper felis vitae erat.
-                        Proin feugiat, augue non elementum posuere, metus purus iaculis lectus, et tristique ligula justo vitae magna.
-                        At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga.
-                        perferendis doloribus asperiores repellat.
-                    </p>
-
-                </div>
-
-                <div className="map text-center">
-
-                    <iframe src="https://www.google.com/maps/d/embed?mid=1FWxXyu67RYMXsJFb_-V6WUuwJ-o&hl=fr"
-                            width="640" height="480" frameBorder="0" style={{border: 0}} allowFullScreen=""
-                            aria-hidden="false" tabIndex="0"/>
-                </div>
-            </div>
+            <TextMap/>
     )
 }
 
