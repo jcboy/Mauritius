7260fbcacc0ba980c05213d2521652d56d41d3b1
SALAH <s.salaheddine91@gmail.com>
2021-01-13 11:31:15 +0100

    add text

---
+++ client/src/pages/ArticleType/ArticleType.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../styles/styles.css';
+
+
+export const ArticleType = () => {
+    return (
+        <div>
+    ²       <h2 className="h2">titre de l'article</h2>
+        </div>
+    )
+}
