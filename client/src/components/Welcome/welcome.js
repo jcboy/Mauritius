80ea8aa524336da0f9a4bdd8aecd9cbadb355f0e
aigouassel <audreyigouassel@gmail.com>
2021-01-14 00:33:03 +0100

    add text

--- client/src/components/Welcome/welcome.js
+++ client/src/components/Welcome/welcome.js
@@ -37,7 +37,7 @@ export class Welcome extends Component {
     }
 
     headerSize(route) {
-        const mainHeaderSize = {marginTop: '12%', marginBottom: 140};
+        const mainHeaderSize = {marginTop: '12%', marginBottom: '12%'};
         const subHeaderSize = {marginTop: 70, marginBottom: 70};
         return route.path === '/' ? mainHeaderSize : subHeaderSize;
     }
