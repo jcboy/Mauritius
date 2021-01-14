f3629da439ec77e7149adf1874c2ec7a3817da4e
aigouassel <audreyigouassel@gmail.com>
2021-01-14 10:45:33 +0100

    tentative de merge

--- client/src/components/Welcome/welcome.js
+++ client/src/components/Welcome/welcome.js
@@ -38,7 +38,6 @@ export class Welcome extends Component {
 
     headerSize(route) {
         const mainHeaderSize = {marginTop: '9.6em', marginBottom: '9.6em'};
-
         const subHeaderSize = {marginTop: 70, marginBottom: 70};
         return route.path === '/' ? mainHeaderSize : subHeaderSize;
     }
