8d1476d49a4066ae39a686c4fde239dbb56526e0
LENOVO <kayaam01@hotmail.fr>
2021-01-15 17:10:10 +0100

    add text

--- client/src/pages/News/News.js
+++ client/src/pages/News/News.js
@@ -2,13 +2,13 @@ import React, {useState} from "react";
 import {Component} from "react";
 import './../../styles/activities.css';
 import thumb01 from "../../assets/images/thumb-01.jpg";
-/*import {Link} from "react-router-dom";*/
+import {Link} from "react-router-dom";
 
 export const News = () => {
 
 
     const [news, setNews] = useState([
-        {id: 1, title: 'actu', date: "05/11/1997", text: 'oikdzfjplsdplsld psdplplsdl plsdpl', link: "okdfokok"},
+        {id: 1, title: 'actu', date: "05/11/1997", text: 'oikdzfjplsdplsld psdplplsdl plsdpl', link: "https://www.vanilla-islands.org/les-iles/ile-maurice/"},
         {id: 2, title: 'actu', date: "07/03/1964", text: 'oikdzfj odsokdok doksokdsk kodsodok', link: "odokfo"},
         {id: 3, title: 'actu', date: "03/11/1999", text: 'oikdzfj dsodkosd kodskdokds okdsodsk ', link: "okfdsok"},
         {id: 4, title: 'actu', date: "09/051997", text: 'oikdzfjfd kfdodfok dokjfkdfo dfokdof', link: "okfdkf"},
@@ -29,30 +29,22 @@ export const News = () => {
             {news.map(item => (
                 <div className="item  mb-5" key={news}>
                     <div className="col-md-8 desc-content">
-                        <div className="wrapper ">
+                        <div className="wrapper">
+                            <div className="row">
+                                <div className="col-md-4 img-content">
+                                    <img src={thumb01} alt=""/>
+                                </div>
+                            </div>
                             <h3 className="h3 red">
                                 {item.title}
                             </h3>
                             <p>Date : {item.date} </p>
+                            <p>text: {item.text} </p>
+                            <Link to={item.link}>&gt; Voir l'article </Link>
                         </div>
                     </div>
                 </div>
 
-
-                /*
-
-                    <div className="row">
-                        <div className="col-md-4 img-content">
-                            <img src={thumb01} alt=""/>
-                        </div>
-
-
-                                <p>
-
-                                </p>
-                                <Link to="/">&gt; Voir l'article</Link>
-                            </div>
-                */
 
 
             ))};
