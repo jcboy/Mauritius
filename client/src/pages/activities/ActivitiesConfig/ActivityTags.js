79e971dd972547e92a8817faa7622f2a0e128b8d
aigouassel <audreyigouassel@gmail.com>
2021-01-17 01:53:23 +0100

    Mise à jour de la page Activities avec la création d'un composant ActivityMapping et ActivityList

--- client/src/pages/activities/ActivitiesConfig/ActivityTags.js
+++ client/src/pages/activities/ActivitiesConfig/ActivityTags.js
@@ -6,34 +6,26 @@ const {Component} = require("react/cjs/react.production.min");
 const categories = ([{id: 1, name: 'tag1'}, {id: 2, name: 'tag2'}, {id: 3, name: 'tag3'}])
 
 
-
 class ActivityTags extends Component {
 
-    constructor(props) {
-        super(props)
-    }
-
     render() {
-        // COMMENCER PAR UNE ROW
         return <div>
-            <div className="filter-content">
-
-                <label htmlFor="exampleDataList" className="form-label">Filtre example</label>
-                <input className="form-control" list="datalistOptions" id="exampleDataList" placeholder="Type to search..." />
-                <datalist id="datalistOptions">
-                    { }
-                </datalist>
-
-                <button type="button" className="btn btn-outline-primary ">Valider</button>
-
-                {/*<button onClick={handleFilter}>Ajouter tag</button>*/}
-            </div>
-            <div>
-                {
-                    categories.map((cat) => {
-                        return <span key={cat.id}>{cat.name}</span>;
-                    })
-                }
+            <div className="row py-4 filter-content">
+                <div className="col-5 d-flex justify-content-center no-wrap">
+                    <input className="form-control"
+                           id="exampleDataList"
+                           style={{width: '50%'}}
+                           placeholder="Type to search..."/>
+                    <button type="button" className="btn btn-outline-primary">Valider</button>
+                    {/*<button onClick={handleFilter}>Ajouter tag</button>*/}
+                </div>
+                <div>
+                    {
+                        categories.map((cat) => {
+                            return <span key={cat.id}>{cat.name}</span>;
+                        })
+                    }
+                </div>
             </div>
         </div>
     }
