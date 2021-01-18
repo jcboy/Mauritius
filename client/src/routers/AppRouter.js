bc56a30750c1d23339a3bba760d192323c3ec420
roche <rochethorelmaxime@gmail.com>
2021-01-18 11:43:45 +0100

    admin liste d'article

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -8,7 +8,7 @@ import {
 import {Home} from "../pages/home/home";
 import {Navbar} from "../components/Navbar/Navbar";
 import ActivityList from "../pages/activities/ActivityList";
-
+import {AdminList} from "../pages/admin-list/Adminlist";
 export const AppRouter = () => {
     return (
         <Router>
@@ -17,10 +17,11 @@ export const AppRouter = () => {
                 <Switch>
                     <Route exact path="/" component={Home} />
                     <Route exact path="/activities" component={ActivityList} />
-
+                    <Route exact path="/adminlist" component={AdminList} />
                     <Redirect to="/" />
                 </Switch>
             </div>
         </Router>
+
     )
 }
