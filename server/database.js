bf7e9333a0f1cb50678c1408a79c95f604d824cf
aigouassel <audreyigouassel@gmail.com>
2021-01-22 18:58:36 +0100

    actualisation du server

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/mauritius-db', {
+mongoose.connect('mongodb://localhost/mauritius', {
     useNewUrlParser : true,
     useUnifiedTopology: true
 })
