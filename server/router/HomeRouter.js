4e584a8c599b2131b7b073a5b7f0b0869c5763e8
aigouassel <audreyigouassel@gmail.com>
2021-01-22 11:40:16 +0100

    actualisation du server

--- server/router/HomeRouter.js
+++ server/router/HomeRouter.js
@@ -5,25 +5,14 @@ const express = require('express');
 const router = express.Router();
 const prefix = '/'
 
-// CRUD
-
-// List columns
 router.get('/', index);
 
-// Create
-// Insertion en bdd
 router.post('/', create);
 
-// Read
-// Read by Id
 router.get('/', read);
 
-// Update
-// Update by Id
 router.put('/', update);
 
-// Delete
-// Delete by Id
 router.delete('/', remove);
 
 module.exports = {router, prefix};
