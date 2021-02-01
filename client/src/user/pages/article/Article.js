8f2088e77a0889f656852c499c5b8c5c095e906b
SALAH <s.salaheddine91@gmail.com>
2021-02-01 19:00:25 +0100

    add text

---
+++ client/src/user/pages/article/Article.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import '../../styles/styles.css';
+import Welcome from "../../components/Welcome/welcome";
+
+
+export const Article = () => {
+    return (
+        <div>
+            <Welcome param={{path: '/'}}/>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-10">
+                        <button type="button" className="btn btn-link">Retour aux articles</button>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <div className="row text-center">
+                            <div className="col-9 mb-4">
+                                <h2 className="h2 green justify-content-center">SOUS-TITRE</h2>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <div className="row text">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat pretium
+                                        risus,
+                                        sit
+                                        amet rhoncus arcu sodales et. Vestibulum ante ipsum primis in faucibus orci
+                                        luctus
+                                        et
+                                        ultrices posuere cubilia curae; Phasellus pulvinar urna velit, vitae dignissim
+                                        mauris
+                                        maximus id. Vestibulum sed vestibulum sem. Fusce maximus eget arcu ac cursus.
+                                        Sed
+                                        facilisis tristique nunc, at tincidunt mauris tempus ut. Vivamus ut ultrices ex.
+                                        Suspendisse potenti.</p>
+
+                                    <p>Quid? qui se etiam nunc subsidiis patrimonii aut amicorum liberalitate
+                                        sustentant,
+                                        hos
+                                        perire patiemur? An, si qui frui publico non potuit per hostem, hic tegitur ipsa
+                                        lege censoria;
+                                        quem is frui non sinit, qui est, etiamsi non appellatur, hostis, huic ferri
+                                        auxilium
+                                        non oportet?
+                                        Retinete igitur in provincia diutius eum, qui de sociis cum hostibus, de civibus
+                                        cum
+                                        sociis faciat
+                                        pactiones, qui hoc etiam se pluris esse quam collegam putet, quod ille vos
+                                        tristia
+                                        voltuque deceperit,
+                                        ipse numquam se minus quam erat, nequam esse simularit. Piso autem alio quodam
+                                        modo
+                                        gloriatur
+                                        se brevi tempore perfecisse, ne Gabinius unus omnium nequissimus
+                                        existimaretur.</p>
+
+                                    <p>Proin ac nunc ac nulla commodo pharetra. Pellentesque habitant morbi tristique
+                                        senectus
+                                        et netus et malesuada fames ac turpis egestas. Proin ac congue metus, egestas
+                                        dapibus
+                                        eros. Aenean at dolor tellus. Fusce sollicitudin sed eros eget malesuada. Mauris
+                                        tempus
+                                        sed lacus nec molestie. Integer sodales tortor luctus varius malesuada.
+                                        Vestibulum
+                                        bibendum, lorem vitae lobortis eleifend, erat odio mattis odio, ut mollis sem ex
+                                        eu
+                                        nibh. Integer rutrum magna in bibendum efficitur. Etiam egestas vitae leo et
+                                        condimentum. Praesent sodales augue sed sagittis consectetur. Donec congue
+                                        ligula
+                                        erat,
+                                        non tempus felis sagittis in.</p>
+                                    <p>
+                                        Iis igitur est difficilius satis facere, qui se Latina scripta dicunt
+                                        contemnere. in
+                                        quibus hoc primum est in quo admirer, cur in gravissimis rebus non delectet eos
+                                        sermo patrius, cum idem fabellas Latinas ad verbum e Graecis expressas non
+                                        inviti
+                                        legant. quis enim tam inimicus paene nomini Romano est, qui Ennii Medeam aut
+                                        Antiopam Pacuvii spernat aut reiciat, quod se isdem Euripidis fabulis delectari
+                                        dicat, Latinas litteras oderit?
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
