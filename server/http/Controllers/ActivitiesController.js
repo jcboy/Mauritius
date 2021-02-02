19f7c1bbe1a04d15b5653165c184beeff2aa025e
aigouassel <audreyigouassel@gmail.com>
2021-02-02 23:27:28 +0100

    actualisation du server

--- server/http/Controllers/ActivitiesController.js
+++ server/http/Controllers/ActivitiesController.js
@@ -2,52 +2,63 @@ const Activity = require('../../models/Activities');
 
 class ActivitiesController {
     index(req, res) {
-        /*
         const query = Activity.find();
         if (!!req.body) {
             if (!!req.body.title) {
-                query.where('title').equals(req.body.title)
+                query.where('title').equals(req.body.title);
             }
             if (!!req.body.date) {
-                query.where('date').equals(req.body.date)
+                query.where('date').equals(req.body.date);
             }
         }
         query.exec((err, response) => {
             if (!!err) {
                 console.log('An error has occurred : ', err.message);
-                res.status(404).send(err)
+                res.status(404).send(err);
             }
             res.send(response);
-        })
-         */
-        Activity.find((err, response) => {
-            if (!!err) {
-                console.log(err);
-            }
-            res.send(response)
-        })
+        });
     }
 
     create(req, res) {
         const newActivity = new Activity(req.body);
         newActivity.save().then((response) => {
-            res.send(response);
+            res.send({
+                message : 'Activité ajoutée à la base de donnée...',
+                response
+            });
         }).catch((err) => {
             console.log('An error has occurred : ', err.message);
-            res.status(501).send({message : 'Les informations reçues sont incomplètes...'})
-        })
-    }
-
-    read(req, res) {
-        res.send("Read");
+            res.status(501).send({message: 'Les informations reçues sont incomplètes...'});
+        });
     }
 
     update(req, res) {
-        res.send("Update");
+        const params = req.body;
+        Activity.findOneAndUpdate(params, {title: 'AAA'},
+            {new: true}, (err, response) => {
+                console.log(response);
+                if (!!err) {
+                    res.status(404).send(err);
+                }
+                res.send({
+                    message : 'Activité correctement actualisée',
+                    response
+                });
+            });
     }
 
-    delete(req, res) {
-        res.send("Delete");
+    remove(req, res) {
+        const id = req.params.id;
+        Activity.findByIdAndDelete(id, (err, response) => {
+            if (!!err) {
+                res.status(404).send(err);
+            }
+            res.send({
+                message : 'Activité correctement supprimée !',
+                response
+            });
+        });
     }
 }
 
--- server/router/ActivitiesRouter.js
+++ server/router/ActivitiesRouter.js
@@ -1,16 +1,13 @@
 const ActivitiesController = require('../http/Controllers/ActivitiesController');
-const {index, update, delete: remove, read, create} = ActivitiesController;
+const {index, update, remove, create} = ActivitiesController;
 
 const express = require('express');
 const router = express.Router();
 const prefix = '/activities';
 
 router.get('/', index);
-
 router.post('/create', create);
-
 router.put('/update', update);
-
-router.delete('/delete', remove);
+router.delete('/delete/:id', remove);
 
 module.exports = {router, prefix};
