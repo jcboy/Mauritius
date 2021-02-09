e7adff3cd8485dabbdff6ac1b09935a105e6f0b7
aigouassel <audreyigouassel@gmail.com>
2021-02-09 15:25:42 +0100

    pagination API activities

--- client/src/user/pages/activities/ActivitiesConfig/ActivityLogic/paginate.test.js
+++ client/src/user/pages/activities/ActivitiesConfig/ActivityLogic/paginate.test.js
@@ -1,18 +1,10 @@
-const getButtonPanel = (pageIndex, pageIndexMax) => {
-    if (pageIndexMax > 2) {
-        let buttonPanel = Array.from({length: 3},
-            (v, i) => i - 1);
-        const start = (pageIndex < 1) ? 1 : 0;
-        const end = (pageIndex >= pageIndexMax) ? -2 : 0;
-        return buttonPanel.map((buttonIndex) => {
-            return buttonIndex + pageIndex + end + start;
-        })
-    } else if (pageIndexMax < 0) {
-        return [0]
-    } else {
-        return Array.from({length: pageIndexMax + 1},
-            (v, i) => i);
-    }
-}
+const paginate = require('./paginate');
 
-console.log(getButtonPanel(0, 4));
+const currentPage = 3;
+const pageMax = 5;
+
+const result = [2,3,4];
+
+test(`On page ${currentPage} with pageMax ${pageMax}`, () => {
+  expect(paginate(currentPage,pageMax)).toBe(result);
+});
