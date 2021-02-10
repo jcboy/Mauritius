6f4d34161b0d79c6751a96b04b179fad3bca5d5a
aigouassel <audreyigouassel@gmail.com>
2021-02-10 01:42:13 +0100

    pagination API activities

--- client/src/user/components/Activities/TagComponent.js
+++ client/src/user/components/Activities/TagComponent.js
@@ -1,11 +1,28 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TagPreview from "./TagPreview";
 import Select from "react-select/async/dist/react-select.esm";
+import axios from 'axios';
 
 const TagComponent = ({setTags}) => {
 
     const [tagList, updateTagList] = useState([]);
     const [input, saveInput] = useState('')
+
+    const [data, setData] = useState([]);
+
+    useEffect( () => {
+        axios.get('http://localhost:8080/categories')
+            .then((response) => {
+                setData(response.data);
+            })
+    }, []);
+
+    const [selectedValue, setSelectedValue] = useState([]);
+    const handleSelectChange = (e) => {
+        setSelectedValue(Array.isArray(e) ? e.map(x => x.value) : []);
+    }
+
+    console.log(data)
 
     const saveTag = () => {
         console.log(input)
@@ -39,21 +56,41 @@ const TagComponent = ({setTags}) => {
      */
 
     //loadOptions={promiseOptions}
-    return (
-        <div className="row pb-3 filter-content flex-column">
+    return (<div className="row pb-3 filter-content flex-column">
             <div className="col-5 d-flex pl-5 no-wrap">
-                <Select cacheOptions
-                        defaultValue={[]}
-                        options={tagList.map((filter) => {
-                            return {label: filter, value: filter}
-                        })}
-                        className="text-dark"
-                        name="filters"
-                        onInputChange={(value) => {
-                            console.log(value)
-                            saveInput(value)
-                        }}
-                        placeholder=""/>
+
+                <Select
+                    defaultValue={[]}
+                    name="data"
+                    // value={data.filter(obj => selectedValue.includes(obj.value))}
+                    options={data.map((cat) => {
+                        return {value: cat.name, label: cat.name}
+                    })}
+                    onChange={handleSelectChange}
+                    className="basic-multi-select text-dark form-control"
+                    classNamePrefix="select"
+                    placeholder=""
+                    isMulti
+                />
+                {selectedValue}
+                {
+                    /*
+                    <Select cacheOptions
+                            defaultValue={[]}
+                            options={tagList.map((filter) => {
+                                return {label: filter, value: filter}
+                            })}
+                            className="text-dark"
+                            name="filters"
+                            onInputChange={(value) => {
+                                console.log(value)
+                                saveInput(value)
+                            }}
+                            placeholder=""/>
+                     */
+                }
+
+
                 <button type="button"
                         className="btn btn-outline-primary"
                         onClick={saveTag}
