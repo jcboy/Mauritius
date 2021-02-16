c2d781e30c396426ee7b9dc532c987a97eb9ee0e
SALAH <s.salaheddine91@gmail.com>
2021-02-16 11:32:45 +0100

    add text

---
+++ client/src/admin/dashboard/addContent/AddContent.js
@@ -0,0 +1,172 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import Select from 'react-select';
+
+import {Sidebar} from "../../common/Sidebar";
+
+import ButtonFileUploadAddContent from "../../common/ButtonFileUploadAddContent/ButtonFileUploadAddContent";
+import ButtonFileUploadAddContentOtherImages
+    from "../../common/ButtonFileUploadAddContentOtherImages/ButtonFileUploadAddContentOtherImages";
+
+export const AddContent = () => {
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        axios.get('http://localhost:8080/categories')
+            .then((response) => {
+                setData(response.data);
+            })
+    }, []);
+
+    const [inputValues, setInputValues] = useState({title: '', subtitle: '', shortDescription: '', description: ''});
+    const handleInputChange = ({target}) => {
+        setInputValues({...inputValues, [target.name]: target.value})
+    }
+
+    const [selectedValue, setSelectedValue] = useState([]);
+    const handleSelectChange = (e) => {
+        setSelectedValue(e.map(x => x.value));
+    }
+
+    const [emptyInput, setEmptyInput] = useState(false);
+    const reset = (value) => emptyInput === true ? [] : value;
+    const handleReset = () => {
+        setSelectedValue([]);
+
+        setEmptyInput(!emptyInput);
+        reset();
+        setEmptyInput(!emptyInput);
+
+        setInputValues({title: '', subtitle: '', shortDescription: '', description: ''});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (inputValues.title.trim().length > 3) {
+            axios.post('http://localhost:8080/actualities', {...inputValues, filters: selectedValue})
+                .then((res) => {
+                    console.log(res.status);
+                    handleReset()
+                })
+        }
+    }
+
+    const [contentType, setContentType] = useState('activities');
+
+    return (
+        <div className="blockContact-AddContent pt-3 container-fluid">
+            <div className="row">
+                <Sidebar/>
+                <main className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                    <h2>Nouveau contenu - {contentType}</h2>
+                    <div className="px-2">
+                        <form>
+                            <div className="row mb-3">
+                                <div className="col-md-4 form-floating">
+                                    <input type="text"
+                                           name="title"
+                                           value={inputValues.title}
+                                           onChange={handleInputChange}
+                                           className="grey form-control" placeholder="Titre"/>
+                                    <label>Titre</label>
+                                </div>
+                                <div className="col-md-4 form-floating">
+                                    <input type="text"
+                                           name="subtitle"
+                                           value={inputValues.subtitle}
+                                           onChange={handleInputChange}
+                                           className="grey form-control"
+                                           placeholder="Sous-titre"/>
+                                    <label>Sous-titre</label>
+                                </div>
+                                <div className="col-md-4 form-floating">
+                                    <select name="typeOfContent" onChange={(e) => setContentType(e.target.value)}
+                                            className="form-select grey"
+                                            id="monselect">
+                                        <option value="activities">article</option>
+                                        <option value="actualities">actu</option>
+                                    </select>
+                                    <label>Sélectionner type de contenu ( article ou actu )</label>
+                                </div>
+                            </div>
+
+                            <div className="row mb-3">
+                                {contentType === 'activities' && (
+                                    <div className="col-md-4 form-floating mb-4">
+                                        <label htmlFor="exampleDataList" className="form-label">Ajouter des filtres à
+                                            votre article</label>
+                                        <Select
+                                            defaultValue={[]}
+                                            name="data"
+                                            value={reset()}
+                                            options={data.map((cat, index) => {
+                                                return {value: cat.name, label: cat.name}
+                                            })}
+                                            onChange={handleSelectChange}
+                                            className="basic-multi-select text-dark form-control"
+                                            classNamePrefix="select"
+                                            placeholder=""
+                                            isMulti
+                                        />
+                                    </div>
+                                )
+                                }
+
+                                <div className="col-md-4 form-floating">
+                                    <button type="button" className="pj"><ButtonFileUploadAddContent/></button>
+                                </div>
+
+                            </div>
+                            <div className="row my-4">
+                                <div className="col-md-8 form-floating shortDescription">
+                                    <textarea name="shortDescription"
+                                              onChange={handleInputChange}
+                                              value={inputValues.shortDescription}
+                                              className="grey form-control" placeholder="Courte description"/>
+                                    <label>Courte description</label>
+                                </div>
+                            </div>
+                            <div className="row mb-4">
+                                <div className="col form-floating">
+                                    <textarea name="description"
+                                              onChange={handleInputChange}
+                                              value={inputValues.description}
+                                              className="grey form-control" placeholder="Contenu"/>
+                                    <label>Contenu</label>
+                                </div>
+                            </div>
+
+                            <div className="row mb-3">
+                                <div className="col-4">
+
+                                    <button type="button" className="pj2"><ButtonFileUploadAddContentOtherImages/>
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="row ">
+                                <div className="offset-8 col text-end">
+                                    <button onClick={handleReset} type="button"
+                                            className="btn btn-outline m-e-4">Annuler
+                                    </button>
+
+                                    <button onClick={handleSubmit} type="button" className="btn-primary btn ">Valider
+                                    </button>
+                                </div>
+                            </div>
+
+
+                        </form>
+                    </div>
+                </main>
+                <div className="row footer">
+                    <div className=" col text-center">
+                        <a className="nav-link white" href="https://fr.lipsum.com/feed/html">Copyright 2020 -
+                            mentions légales</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
