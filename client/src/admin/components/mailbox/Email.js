9983d3f19b4973a4de0f67d27d3d15b77cca7027
aigouassel <audreyigouassel@gmail.com>
2021-03-04 17:15:06 +0100

    add text

--- client/src/admin/components/mailbox/Email.js
+++ client/src/admin/components/mailbox/Email.js
@@ -8,10 +8,7 @@ export const Email = () => {
             <div className="row">
                 <Sidebar />
                 <main className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-
                     Email
-
-
                 </main>
             </div>
         </div>
