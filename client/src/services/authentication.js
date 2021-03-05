29ec58bad13024a5f2c6dfa5405867ae64ead108
jcboyG4C <jcboy@goforcolor.com>
2021-03-05 12:35:56 +0100

    style page change password

--- client/src/services/authentication.js
+++ client/src/services/authentication.js
@@ -2,14 +2,12 @@ import admin from "./admin";
 
 class Authentication {
 
-    async login(userCredentials, getIn, getOut) {
+    async login(userCredentials, getIn) {
         const {data, headers} = await admin.getAdmin(userCredentials);
         if (!!headers.authorization) {
             localStorage.setItem("authorization", headers.authorization.split(" ")[1]);
             localStorage.setItem("userId", data._id);
             return getIn();
-        } else {
-            return getOut();
         }
     }
 
--- client/src/user/pages/Login.js
+++ client/src/user/pages/Login.js
@@ -5,17 +5,17 @@ export const Login = (props) => {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [alert, setAlert] = useState(false);
 
     const login = async (e) => {
         e.preventDefault();
         try {
-            await auth.login(
-                {email, password},
+            await auth.login( {email, password},
                 () => {
                     props.history.push("/admin");
-                },
-                () => alert("Wrong password"));
+                })
         } catch(err) {
+            setAlert(!alert)
             console.log(err);
         }
     }
@@ -27,14 +27,14 @@ export const Login = (props) => {
                     Mauritius
                 </div>
 
-                <div className="form-floating mb-2">
+                <div className="form-floating mb-2 error">
                     <input type="email" name="email" placeholder="Email2"
                            className="form-control mb-1" required
                            onChange={(event) => {
                                setEmail(event.target.value);
                            }}/>
                     <label htmlFor="floatingInput">Email</label>
-                    {/* <span className="text-danger fst-italic mt-1">Email2 incorrect</span> */}
+                    {alert && <span className="text-danger fst-italic mt-1">Email incorrect</span>}
                 </div>
 
                 <div className="form-floating mb-3">
@@ -44,7 +44,7 @@ export const Login = (props) => {
                                setPassword(event.target.value)
                            }}/>
                     <label>Mot de passe</label>
-                    {/* <span className="text-danger fst-italic">Mot de pass incorrect</span> */}
+                    {alert && <span className="text-danger fst-italic">Mot de passe incorrect</span> }
                 </div>
                 <div className=" mt-5 pt-2">
                     <button type="submit" className="btn w-100"
